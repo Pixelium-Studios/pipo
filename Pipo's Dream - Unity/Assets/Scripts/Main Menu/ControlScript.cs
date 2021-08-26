a1fb31edb5fe9cf76576eeda15981363a3c606a9
Danijel <danijel.pirc96@gmail.com>
2021-08-26 20:07:42 +0200

    zavrsen basic v1 GUI, dodan todo.txt

--- Pipo's Dream - Unity/Assets/Scripts/Main Menu/ControlScript.cs
+++ Pipo's Dream - Unity/Assets/Scripts/Main Menu/ControlScript.cs
@@ -8,35 +8,51 @@ public class ControlScript : MonoBehaviour
     private GameObject video_panel_loading;
     [SerializeField]
     private GameObject video_panel_start_menu;
-
-
     [SerializeField]
-    private VideoClip startMenuClip;
-    [SerializeField]
-    private VideoPlayer myVideoPlayer;
+    private float initialPlayDelay = 1.5f;
     [SerializeField]
     private RawImage myRawImage;
-
+    [SerializeField]
+    private VideoPlayer loading_video_player;
+    [SerializeField]
+    private VideoPlayer start_menu_video_player;
+    [SerializeField]
+    private Button startButton;
+    
     // Start is called before the first frame update
     void Start()
     {
-        //Invoke("InvokedFunction",2f);
+        loading_video_player.loopPointReached += StartNewClip;
 
-        myVideoPlayer.loopPointReached += StartNewClip;
-        myVideoPlayer.Play();
-        myRawImage.enabled = true;
+        // do delayed start ( zbog onih vizualnih problema pri Play-anju Unity programa )
+
+        // izgleda da nije dovoljno staviti delay pa onda SetActive na cijeli panel, i dalje "freeza" neki frame iz videa kad se aktivira panel pa onda krene playati normalno
+        // mozda playati video a imati "deaktiviran" Image, pa "pecati" tajming da se aktivira taman kad se video pocne "normalno playati"
+        Invoke("InvokedFunction", initialPlayDelay);    
     }
 
-    //void InvokedFunction()
-    //{
-    //    openingAnimationPanel.SetActive(true);
-    //}
+    void InvokedFunction()
+    {
+        video_panel_loading.SetActive(true);
+        loading_video_player.Play();
+    }
 
     void StartNewClip(VideoPlayer myVidPlayer)
     {
+        // myVidPlayer je od prethodnog panela, NIJE ISTI VideoPlayer kao na 2nd panelu
+
         video_panel_loading.SetActive(false);
         video_panel_start_menu.SetActive(true);
+        startButton.gameObject.SetActive(true);
+
+        start_menu_video_player.Play();
+
         //myVideoPlayer.loopPointReached -= StartNewClip;
+    }
+
+    public void OnStartButtonClick()
+    {
+        Debug.Log("start button click registered!");
     }
 
    
